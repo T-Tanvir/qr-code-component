e1f4dc50ba5a6c635a2f6d7eb728929d4563fd85
Tanvir <111074686+T-Tanvir@users.noreply.github.com>
2024-02-24 13:31:36 +0600

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./doc/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
